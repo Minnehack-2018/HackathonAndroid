6724390dc903189aec53822e92e01b34c47e82c8
Jude Ibe <judeibe@gmail.com>
2018-01-28 03:52:24 -0600

    Updated AppDatabase.java
    Singleton

--- app/src/main/java/winningteam/minnehack/io/hackathonandroid/db/AppDatabase.java
+++ app/src/main/java/winningteam/minnehack/io/hackathonandroid/db/AppDatabase.java
@@ -1,7 +1,9 @@
 package winningteam.minnehack.io.hackathonandroid.db;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 import model.User;
 import model.WarmUp;
@@ -10,6 +12,25 @@ import winningteam.minnehack.io.hackathonandroid.db.dao.WarmUpDao;
 
 @Database(entities = {WarmUp.class, User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+    private static final String DATABASE = "warm-up.db";
+    private static AppDatabase INSTANCE = null;
+
     public abstract UserDao userDao();
     public abstract WarmUpDao warmupDao();
+
+    public static AppDatabase getInstance(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, DATABASE)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public static void destoryInstance() {
+        INSTANCE = null;
+    }
 }
